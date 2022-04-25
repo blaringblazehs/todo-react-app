69e20ef0cd0a915f0ef35dcc9a17588d96f04c41
Himanshu Soni <39999787+blaringblazehs@users.noreply.github.com>
2022-04-25 13:26:24 +0530

    first commmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [inputList, setInputList] = useState("");
+    const [items, setItems] = useState([]);
+
+    const itemEvent = (e) => {
+        setInputList(e.target.value);
+    };
+    const addItem = () => {
+        if (inputList === "") return;
+        setItems((oldItem) => {
+            return [...oldItem, inputList];
+        });
+
+        //to empty the input field
+        setInputList("");
+    };
+
+    return (
+        <div className="App">
+            <div className="container">
+                <br />
+                <div className="head">
+                    <h1>ToDo List</h1>
+                </div>
+
+                <br />
+                <div className="input-div">
+                    <input
+                        type="text"
+                        placeholder="Add an Item.."
+                        value={inputList}
+                        onChange={itemEvent}
+                    />
+                    <button onClick={addItem}>+</button>
+                </div>
+
+                <ul>
+                    {/* <li>{inputList}</li> */}
+                    {items.map((item) => {
+                        return <li>{item}</li>;
+                    })}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
